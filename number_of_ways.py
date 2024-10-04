a661716c0ba9499ca201b49ad6105c4ec75001c9
Andy R <andymrobertsuk@gmail.com>
2024-10-04 16:55:52 +0100

    Able to run but leetcodes says too slow. Need to modify to dynamic programming method.

--- number_of_ways.py
+++ number_of_ways.py
@@ -17,30 +17,33 @@ def numberOfWays(startPos: int, endPos: int, k: int) -> int:
     """
     # start with path of length 1
     paths = [startPos]
+    
 
     # loop k times
     for i in tqdm(range(k)):
-        for path in paths:
-            new_path = path.copy()
-            last_position = new_path[-1]
+        new_paths = []
+        for last_position in paths:
 
-            # exist fast if not going to make to end
-            if endPos - last_position > (k - i - 1):
-                continue
+            # exit fast if not going to make to end
+            # if difference > (remaining steps)
+            if endPos - last_position > (k - i + 1):
+               continue
             # path that goes to the left
-            new_path_left = new_path + [last_position - 1]
+            new_path_left = last_position - 1
 
             # path that goes to the right
-            new_path_right = new_path + [last_position - 1]
+            new_path_right = last_position + 1
 
             # add paths to the left and right
-            paths.append(new_path_left)
-            paths.append(new_path_right)
+            new_paths.append(new_path_left)
+            new_paths.append(new_path_right)
+        
+        paths = new_paths
 
     num_ways = 0
     for path in paths:
-        if path[-1] == endPos:
-            new_ways += 1
+        if path == endPos:
+            num_ways += 1
     return num_ways
 
 
